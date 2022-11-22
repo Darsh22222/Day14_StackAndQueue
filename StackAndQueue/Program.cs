aefe90881d35c8217c14048f4b0c98cb8c073671
Darsh <darshankondhare2698@gmail.com>
2022-11-22 15:57:05 +0530

    [darsh] Add: Peak And Pop

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -8,7 +8,7 @@
             bool condition = true;
             while (condition)
             {
-                Console.WriteLine("1. Pushing the Node to Stack\n2. Exit");
+                Console.WriteLine("1. Pushing the Node to Stack\n2.Peak the Node from the Stack" + "\n3. Pop the Node from the Stack\n4. Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 Stack stacks = new Stack();
                 switch (choice)
@@ -18,6 +18,22 @@
                         stacks.Push(30);
                         stacks.Push(70);
                         stacks.Display();
+                        break;
+                    case 2:
+                        stacks.Push(56);
+                        stacks.Push(30);
+                        stacks.Push(70);
+                        stacks.Peak();
+                        stacks.Display();
+                        Console.WriteLine("\n");
+                        break;
+                    case 3:
+                        stacks.Push(56);
+                        stacks.Push(30);
+                        stacks.Push(70);
+                        stacks.Pop();
+                        stacks.Display();
+                        Console.WriteLine("\n");
                         break;
                     default:
                         condition = false;
