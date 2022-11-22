f33a124f54ddb467451f215e3d02afc240287b28
Darsh <darshankondhare2698@gmail.com>
2022-11-22 15:49:16 +0530

    [darsh] Add: Create A Stack

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to stack and queue problem");
+            bool condition = true;
+            while (condition)
+            {
+                Console.WriteLine("1. Pushing the Node to Stack\n2. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                Stack stacks = new Stack();
+                switch (choice)
+                {
+                    case 1:
+                        stacks.Push(56);
+                        stacks.Push(30);
+                        stacks.Push(70);
+                        stacks.Display();
+                        break;
+                    default:
+                        condition = false;
+                        break;
+                }
+            }
         }
     }
 }
